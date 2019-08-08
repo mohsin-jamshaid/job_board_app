829a1a43914cd1b7de2a497cf197a61d54e03e18
mohsin-jamshaid <mohsin.jamshaid007@gmail.com>
2019-08-08 19:57:54 +0500

    Job table created * "Migration file for job table created" * "attributes of job added in migration file" * "db migrated"

---
+++ db/migrate/20190808144441_create_job.rb
@@ -0,0 +1,11 @@
+class CreateJob < ActiveRecord::Migration[5.2]
+  def change
+    create_table :jobs do |t|
+      t.string :title, default: '', null: false
+      t.integer :category
+      t.string :company, default: '', null: false
+      t.text :description, default: '', null: false
+      t.string :link, default: '', null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_08_08_144441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title", default: "", null: false
+    t.integer "category"
+    t.string "company", default: "", null: false
+    t.text "description", default: "", null: false
+    t.string "link", default: "", null: false
+  end
 
 end
