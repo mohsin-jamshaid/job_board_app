056fccc673f0702901c6b6904d39719db5780316
mohsin-jamshaid <mohsin.jamshaid007@gmail.com>
2019-08-08 20:12:22 +0500

    Job model validation added

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,5 @@
 class Job < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  enum category: %i[full_time part_time freelance remote]
+  validates :company, :link, :description, presence: true
 end
